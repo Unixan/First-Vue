2d75aacfff710c37361dfb98adf5b626224011b5
Unixan <115973884+Unixan@users.noreply.github.com>
2023-02-03 14:53:40 +0100

    Start på vue tutorial
    https://youtu.be/CYPZBK8zUik?t=476

---
+++ app.js
@@ -0,0 +1,30 @@
+console.log("hello, vue!");
+const app = Vue.createApp({
+  // data, funtions
+  // template: '<h2>I am the template</h2>'
+  data() {
+    return {
+      showBooks: true,
+      title: "The Final Empire",
+      author: "Brandon Sanderson",
+      age: 45,
+    };
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    handleEvent(x, data){
+        console.log(x, x.type)
+        if(data){
+            console.log(data)
+        }
+    }
+    // changeTitle(title) {
+    //   // this.title = 'words of radiance'
+    //   this.title = title;
+    // },
+  },
+});
+
+app.mount("#app");
