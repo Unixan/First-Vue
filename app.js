d58a338ef5fff0bd9fc1b33e7a5fc82324426b6a
Unixan <115973884+Unixan@users.noreply.github.com>
2023-02-09 12:31:51 +0100

    Start på første oppgave

--- app.js
+++ app.js
@@ -3,22 +3,45 @@ const app = Vue.createApp({
   // template: '<h2>I am the template</h2>'
   data() {
     return {
+      url: "http://youtube.com",
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
+      books: [
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
     },
-    handleEvent(x, data){
-        console.log(x, x.type)
-        if(data){
-            console.log(data)
-        }
-    }
+    // ,
+    // handleEvent(x, data) {
+    //   console.log(x, x.type);
+    //   if (data) {
+    //     console.log(data);
+    //   }
+    // },
+    // handleMousemove(e) {
+    //   this.x = e.offsetX
+    //   this.y = e.offsetY
+    // },
     // changeTitle(title) {
     //   // this.title = 'words of radiance'
     //   this.title = title;
