329ec58f63209c3b785a5fa8443f437557b0a57e
Unixan <115973884+Unixan@users.noreply.github.com>
2023-02-17 14:50:17 +0100

    ninja-jobs WIP

---
+++ ninja-jobs/src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ipsam
+      nesciunt vitae soluta corrupti quasi quia et accusantium voluptate
+      dolorum, incidunt atque amet minus? Rem consectetur ipsum, doloribus quis
+      recusandae tempore nam laboriosam soluta illo a laborum repellat dolorum
+      accusantium quasi ducimus natus! Numquam impedit vitae distinctio quam,
+      nam praesentium nulla assumenda! Recusandae minus facere molestiae ab cum!
+      Quae aliquid voluptatibus dignissimos mollitia magnam corporis laudantium
+      veritatis excepturi perferendis possimus facilis minima dicta dolore
+      officiis eos quasi est, sint eius eligendi ipsam minus harum ducimus?
+      Facere ducimus quisquam odit alias aut molestias aliquam, commodi amet,
+      assumenda eos dolores omnis vitae!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ipsam
+      nesciunt vitae soluta corrupti quasi quia et accusantium voluptate
+      dolorum, incidunt atque amet minus? Rem consectetur ipsum, doloribus quis
+      recusandae tempore nam laboriosam soluta illo a laborum repellat dolorum
+      accusantium quasi ducimus natus! Numquam impedit vitae distinctio quam,
+      nam praesentium nulla assumenda! Recusandae minus facere molestiae ab cum!
+      Quae aliquid voluptatibus dignissimos mollitia magnam corporis laudantium
+      veritatis excepturi perferendis possimus facilis minima dicta dolore
+      officiis eos quasi est, sint eius eligendi ipsam minus harum ducimus?
+      Facere ducimus quisquam odit alias aut molestias aliquam, commodi amet,
+      assumenda eos dolores omnis vitae!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+};
+</script>
